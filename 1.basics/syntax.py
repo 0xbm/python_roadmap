53cfa8416c462ca5d5c708a80fa8e7062d66f0c8
0xbm <0xmb@protonmail.com>
2023-08-14 20:33:35 +0200

    commit

---
+++ 1.basics/syntax.py
@@ -0,0 +1,32 @@
+print(
+    "1.Python is case sensitive. Hence a variable with name yoyostudytonight is"
+    "not same as yoYoStudytonight.")
+print("2.Forward slashes ex. python program path should be C:/folderA/folderB.")
+print("3.No semicolon (;).")
+print(
+    '''4.In python, you can use single quotes '', double quotes "" and even 
+    triple quotes """ ''' + "'''" + ''' to represent string literals.''')
+print('''
+word = 'word'
+sentence = "This is a one line sentence."
+para = """This is a paragraph 
+which has multiple lines"""
+''')
+print("5.Comments with # at start.")
+print("6.Line Continuation : backslash " + '\\.')
+print("""
+sum =  123 + \
+       456 + \
+       789
+""")
+sum = 123 + \
+      456 + \
+      789
+print(sum)
+print(
+    "7.Blank lines in between a program are ignored by python" + " --> '\\n'.")
+print(
+    "8.Code Indentation: This is the most important rule of python programming."
+    "It is recommended to use tab for indentation, although you can use spaces "
+    "for indentation as well, just keep in mind that the amount of indentation "
+    "for a single code block should be same.")
