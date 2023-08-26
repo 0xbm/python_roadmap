21adbd510a4a183a0b92d7db8d49e1243b34ecf2
0xbm <0xmb@protonmail.com>
2023-08-26 14:47:17 +0200

    commit

--- 1.basics/6a.exercise.py
+++ 1.basics/6a.exercise.py
@@ -3,8 +3,8 @@ wallet = []
 
 Choose = None
 while Choose != '0':
-    Choose = input("Choose number:")
     print("1.Deposit \n2.Withdrawal \n3.Saldo ")
+    Choose = input("Choose number:")
     if Choose == "1":
         cash = input("Add Cash:")
         wallet.append(int(cash))
