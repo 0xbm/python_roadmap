34711aba89d1720a807b23f6b33d88b5e959848d
0xbm <0xmb@protonmail.com>
2023-08-26 14:54:00 +0200

    commit

--- 1.basics/6a.exercise.py
+++ 1.basics/6a.exercise.py
@@ -1,17 +1,18 @@
-print("Simple Wallet")
-wallet = []
+def walletw():
+    print("Simple Wallet")
+    wallet = []
+    Choose = None
+    while Choose != '0':
+        print("1.Deposit \n2.Withdrawal \n3.Saldo ")
+        Choose = input("Choose number:")
+        if Choose == "1":
+            cash = input("Add Cash:")
+            wallet.append(int(cash))
+        if Choose == "2":
+            withdrawal = input("Withdrawal: ")
+            wallet.append(-int(withdrawal))
+        if Choose == '3':
+            print(wallet)
 
-Choose = None
-while Choose != '0':
-    print("1.Deposit \n2.Withdrawal \n3.Saldo ")
-    Choose = input("Choose number:")
-    if Choose == "1":
-        cash = input("Add Cash:")
-        wallet.append(int(cash))
-    if Choose == "2":
-        withdrawal = input("Withdrawal: ")
-        wallet.append(-int(withdrawal))
-    if Choose == '3':
-        print(wallet)
 
-print(wallet)
+walletw()
